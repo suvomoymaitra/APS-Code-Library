34e5e388dcf06ef4841343540b1228cec58b7bf5
Suvomoy Maitra <54491482+suvomoymaitra@users.noreply.github.com>
2022-04-07 01:33:01 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/
+// Concept used : Dynamic programming
+class Solution {
+public:
+    int lis(vector<int> const& a) {
+    int n = a.size();
+    const int INF = 1e9;
+    vector<int> d(n+1, INF);
+    d[0] = -INF;
+
+    for (int i = 0; i < n; i++) {
+        int j = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[j-1] < a[i] && a[i] < d[j])
+            d[j] = a[i];
+    }
+
+    int ans = 0;
+    for (int i = 0; i <= n; i++) {
+        if (d[i] < INF)
+            ans = i;
+    }
+    return ans;
+}
+    int lengthOfLIS(vector<int>& nums) {
+        return lis(nums);
+    }
+};
