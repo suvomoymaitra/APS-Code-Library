456ffc5889d6728a5d8d581e6d2da51001152a39
Suvomoy Maitra <54491482+suvomoymaitra@users.noreply.github.com>
2022-04-07 02:04:10 +0530

    Create Collision.cpp

---
+++ Collision.cpp
@@ -0,0 +1,85 @@
+// https://atcoder.jp/contests/abc209/tasks/abc209_d
+// Concept used : Bipartite graph using depth first search
+//  299143BT
+#include<bits/stdc++.h>
+#define int long long int
+#define MOD 1000000007
+#define pi 3.1415926535
+#define fi first
+#define se second
+#define vll vector<int>
+#define vpll vector<pair <int,int> >
+#define vvll vector< vll >
+#define str string
+#define umap unordered_map
+#define uset unordered_set
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repA(i,s,n) for(int i=s;i<=n;i++)
+#define repD(i,s,n) for(int i=s;i>=n;i--)
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define elif else if
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ALL(v) v.begin(),v.end()
+#define sortA(v) sort(ALL(v))
+#define sortD(v) sort(ALL(v),greater <int> ())
+#define print(v) for(auto itr:v) cout<<itr<<" ";
+#define sz size()
+#define nl cout<<"\n";
+using namespace std;
+int factorial(int n){return (n==1||n==0)?1:n*factorial(n-1);}
+int power(int x,int y){int res=1;while(y>0){if(y&1)res=res*x;y=y>>1;x=x*x;}return res;}
+int powerMod(int x,int y,int m){if(y==0)return 1;int p=powerMod(x,y/2,m)%m;p=(p*p)%m;return (y%2==0)?p:(x*p)%m;}
+int modInverse(int a,int m){return powerMod(a,m-2,m);}
+int gcd(int a,int b){if(a==0){return b;}else if(b==0){return a;}else{if(a>b){return gcd(a%b,b);}else{return gcd(b%a,a);}}}
+
+void pre(){}
+
+vll v[100046];
+int visited[100046]={0};
+int arr[100046];
+
+void dfsedge(int a,int b){
+    v[a].pb(b);
+    v[b].pb(a);
+}
+void dfs(int a,int x){
+    if(visited[a]==1)
+        return;
+    visited[a]=1;
+    arr[a]=x;
+    // cout<<a<<" "<<arr[a]<<" ";
+    for(auto it:v[a]){
+        dfs(it,1^x);
+    }
+}
+
+void solve(int t) {
+    int  n=0,q=0;
+    memset(visited,0,sizeof(visited));
+    cin>>n>>q;
+    rep(i,n-1){
+        int a=0,b=0;
+        cin>>a>>b;
+        dfsedge(a,b);
+    }
+    dfs(1,1);
+    int t1,t2;
+    while(q--){
+        cin>>t1>>t2;
+        if(arr[t1]==arr[t2]) cout<<"Town";
+        else cout<<"Road";
+        nl
+    }
+}
+
+int32_t main(){
+    fast
+    int t=1;
+//    cin>>t;
+    pre();
+    rep(i,t) solve(t);
+    return 0;
+}
